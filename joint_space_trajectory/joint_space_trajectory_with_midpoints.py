194fcb5c497ac686b3a2ae15e67666b6bed2ece2
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-12-23 23:54:53 +0100

    #1 Implemented new trajectory solver
    for trajectory generation in joint space with any number of midpoints

---
+++ joint_space_trajectory/joint_space_trajectory_with_midpoints.py
@@ -0,0 +1,102 @@
+from kinematics.kinematics import KinematicsSolver
+from joint_trajectories import MidpointsJointSpaceTrajectory
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+# Wszystkie wartości translacji w metrach
+# Translacja mocowania - pomiędzy world frame, a J1
+mount_t = np.array([0, 0, 0.05, 1])
+
+# Translacja T1 - pomiędzy J1, a J2
+t1 = np.array([0.05, 0, -0.035, 1])
+
+# Translacja T2 - pomiędzy J2, a J3
+t2 = np.array([0.09, 0, 0, 1])
+
+# Translacja T3 - pomiędzy J3, a FCP
+t3 = np.array([0.1, 0, 0, 1])
+
+
+kinematics_solver = KinematicsSolver(mount_t, t1, t2, t3, 0, -np.pi/2)
+midpoints_trajectory = MidpointsJointSpaceTrajectory(kinematics_solver)
+
+# 5 interpolation points (2 boundary points and 3 midpoints), each represented as subarray with values in x, y and z axis
+coordinates = np.array([[0, -0.05, 0], [0.05, -0.02, 0.05], [0.05, 0, 0.06], [0.05, 0.02, 0.05], [0, 0.05, 0]])
+
+# Boundary velocities (start and finish velocity) for each joint
+velocities = np.array([[0,0], [0,0], [0,0]])
+
+# Move times meaning time for each joint to move from currently starting point to the next point
+move_times = np.array([1.5, 0.25, 0.25, 1.5])
+
+# Time steps for whole movement probed with f=200Hz (T=5ms)
+x = np.arange(0, sum(move_times), 0.005)
+
+
+polys = midpoints_trajectory.generate_trajectory(coordinates, velocities, move_times)
+
+
+midtrajectory_positions = polys[0](x)
+midtrajectory_vels = polys[0].get_velocity(x)
+midtrajectory_accels = polys[0].get_acceleration(x)
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+
+# Plot data on the first subplot
+ax1.plot(x, midtrajectory_positions, color='blue')
+ax1.set_title("Joint 0 position relative to timesteps")
+ax1.set_xlabel("Timesteps")
+ax1.set_ylabel("Position [rad]")
+ax1.grid()
+
+# Plot data on the second subplot
+ax2.plot(x, midtrajectory_vels, color='green')
+ax2.set_title("Joint 0 velocity relative to timesteps")
+ax2.set_xlabel("Timesteps")
+ax2.set_ylabel("Velocity [rad/s]")
+ax2.grid()
+
+# Plot data on the third subplot
+ax3.plot(x, midtrajectory_accels, color='red')
+ax3.set_title("Joint 0 acceleration relative to timesteps")
+ax3.set_xlabel("Timesteps")
+ax3.set_ylabel("Acceleration [rad/s^2]")
+ax3.grid()
+
+# Adjust layout to prevent overlap
+plt.tight_layout()
+
+x_points = []
+y_points = []
+z_points = []
+
+theta1 = polys[0](x)
+theta2 = polys[1](x)
+theta3 = polys[2](x)
+for i in range(len(x)):
+    forward_coordinates = kinematics_solver.forward(theta1[i], theta2[i], theta3[i])
+    x_points.append(forward_coordinates[0])
+    y_points.append(forward_coordinates[1])
+    z_points.append(forward_coordinates[2])
+
+# Create a 3D scatter plot
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title(label="Trajectory with 2 midpoints generated with spline polynomial")
+scatter = ax.scatter(x_points, y_points, z_points, marker='o', label="Spline polynomial")
+ax.view_init(elev=45, azim=135)
+coordinate_points_x = [x[0] for x in coordinates]
+coordinate_points_y = [x[1] for x in coordinates]
+coordinate_points_z = [x[2] for x in coordinates]
+ax.scatter(coordinate_points_x, coordinate_points_y, coordinate_points_z, color='red', s=100, label="Interpolated points")
+
+# Add labels
+ax.set_xlabel('X Axis [m]')
+ax.set_ylabel('Y Axis [m]')
+ax.set_zlabel('Z Axis [m]')
+ax.legend()
+
+# Show the interactive plot
+plt.show()
--- joint_space_trajectory/joint_trajectories.py
+++ joint_space_trajectory/joint_trajectories.py
@@ -1,5 +1,7 @@
 from kinematics.kinematics import KinematicsSolver
 import numpy as np
+import math
+from typing import Tuple
 
 
 class SimpleJointSpaceTrajectory:
@@ -59,6 +61,125 @@ class SimpleJointSpaceTrajectory:
                       [0, 1, 2 * end_time, 3 * pow(end_time, 2)]]
                      )
         b = np.array([start_angle, end_angle, start_velocity, end_velocity])
+
+        a = np.linalg.solve(A, b)
+        return a
+
+
+class SplinePolynomial:
+    def __init__(self, coefficients_list: np.ndarray, time_limits: np.ndarray):
+        self._coefficients_list = coefficients_list
+        self._time_limits = time_limits
+        self._calculated_time_limits = [sum(self._time_limits[:i]) for i in range(1, len(self._time_limits)+1)]
+
+    def __call__(self, time_steps: np.ndarray) -> float:
+        values = []
+        for time_step in time_steps:
+            poly, polynumber = self._select_polynomial(time_step)
+
+            values.append(
+                self._generate_polynomial_value(poly, time_step, polynumber))
+
+        return np.array(values)
+
+    def _select_polynomial(self, time_step: float) -> Tuple[np.poly1d, int] | None:
+        for i, time_limit in enumerate(self._calculated_time_limits):
+            if time_step < time_limit:
+                polynumber = i
+
+                poly = np.poly1d(np.flip(self._coefficients_list[polynumber]))
+                return poly, polynumber
+        return None
+    
+    def _generate_polynomial_value(self, poly: np.poly1d, time_step: float, polynumber: int) -> float:
+        if polynumber == 0:
+            return poly(time_step)
+        else:
+            return poly(
+                time_step - self._calculated_time_limits[polynumber - 1])
+
+    def get_velocity(self, time_steps: np.ndarray) -> float:
+        values = []
+        for time_step in time_steps:
+            poly, polynumber = self._select_polynomial(time_step)
+            poly = np.polyder(poly)
+
+            values.append(
+                self._generate_polynomial_value(poly, time_step, polynumber))
+        return np.array(values)
+
+    def get_acceleration(self, time_steps: np.ndarray) -> float:
+        values = []
+        for time_step in time_steps:
+            poly, polynumber = self._select_polynomial(time_step)
+            poly = np.polyder(poly, 2)
+
+            values.append(
+                self._generate_polynomial_value(poly, time_step, polynumber))
+        return np.array(values)
+
+
+class MidpointsJointSpaceTrajectory:
+    def __init__(self, kinematics_solver: KinematicsSolver):
+        self._kinematics_solver = kinematics_solver
+        self._poly_deg = 3
+
+    def generate_trajectory(self, coordinates: np.ndarray, velocities: np.ndarray, time_steps: np.ndarray) -> list:
+        thetas1 = []
+        thetas2 = []
+        thetas3 = []
+        for coordinate in coordinates:
+            start_position = np.array([*coordinate, 1])
+            angles = self._kinematics_solver.inverse(start_position)
+            thetas1.append(angles[0])
+            thetas2.append(angles[1])
+            thetas3.append(angles[2])
+
+        joint_angles = [thetas1, thetas2, thetas3]
+
+        polys = []
+        for i in range(3):
+            coefficients = self._generate_polynomial_coefficients(joint_angles[i], velocities[i], time_steps)
+            coeffs_split = np.array_split(coefficients, len(coefficients) // (self._poly_deg+1))
+            coeffs_split = [coeffs.reshape((4,)) for coeffs in coeffs_split]
+            polys.append(SplinePolynomial(coeffs_split, time_steps))
+
+        return polys
+
+    def _generate_polynomial_coefficients(self, angles: np.ndarray, velocities: np.ndarray,  time_steps: np.ndarray) -> np.ndarray:
+        polys = len(time_steps)
+        # A = np.zeros((polys*(self._poly_deg+1)))
+        A = np.zeros((polys*(self._poly_deg+1), polys*(self._poly_deg+1)))
+
+        # Fill the A matrix with coefficients from position equations
+        for i in range(polys):
+            A[i*2, i*4] = 1
+            for j in range(self._poly_deg+1):
+                A[i * 2 + 1, i * 4 + j] = pow(time_steps[i], j)
+
+        A[2*polys, 1] = 1
+        for j in range(self._poly_deg):
+            A[2*polys + 1, (polys - 1) * 4 + 1 + j] = (1+j)*pow(time_steps[-1], j)
+
+        for i in range(polys-1):
+            for j in range(self._poly_deg):
+                A[polys*2+2+i, i*4+1 + j] = (1+j)*pow(time_steps[i], j)
+            A[polys*2+2+i, 1 + (1+i)*4] = -1
+
+        for i in range(polys-1):
+            for j in range(self._poly_deg-1):
+                A[polys*3+1+i, i*4+2 + j] = math.factorial(2+j) * pow(time_steps[i], j)
+            A[polys*3+1+i, 2 + (1+i)*4] = -2
+
+        b = np.zeros((polys*4, 1))
+        b[0] = angles[0]
+        for i in range(len(angles)-2):
+            b[1+i*2] = angles[1+i]
+            b[2+i*2] = angles[1+i]
+
+        b[1+(len(angles)-2)*2] = angles[-1]
+        b[2 + (len(angles) - 2) * 2] = velocities[0]
+        b[3 + (len(angles) - 2) * 2] = velocities[1]
 
         a = np.linalg.solve(A, b)
         return a
